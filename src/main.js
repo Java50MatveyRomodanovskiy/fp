86de037a12e9725f3adeda8824dba6d6a35b705d
MatveyRomodanovskiy <startpolus@gmail.com>
2023-02-16 10:27:41 +0300

    without css

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 import { weatherConfig } from "./config/weather-config.js";
 import { DataProcessor } from "./service/DataProcessor.js";
 import { DataForm } from "./ui/data-form.js";
+import { Table } from "./ui/table.js";
 const url = "https://api.open-meteo.com/v1/gfs?hourly=temperature_2m&timezone=IST";
 const dataProcessor = new DataProcessor(url, weatherConfig.cities);
-//data fo testing
-const startDate = '2023-02-13'
-const endDate = '2023-02-15'
-const hourFrom = 10;
-const hourTo = 20;
+
 const cities = weatherConfig.cities;
 
 //end data for testing
-// async function displayTemperatures() {
-//     const data = await dataProcessor.getTemperatureData(city, startDate , endDate, hourFrom, hourTo);
-//     data.forEach(e => console.log(e));
-// }
-// displayTemperatures();
-const dataForm = new DataForm("form-section", 16, cities);
-dataForm.addFormHandler(startFunction);
-function startFunction(){
-    console.log('end');
+async function displayTemperatures(dataForm) {
+    const {city} = dataForm;
+    const {dateTo} = dataForm;
+    const {dateFrom} = dataForm;
+    const {hourFrom} = dataForm;
+    const {hourTo} = dataForm;
+    const data = await dataProcessor.getTemperatureData(city, dateTo , dateFrom, hourFrom, hourTo);
+    data.forEach(e => tableForecast.addRow(e));
 }
+const schema = [
+    {columnName: 'Date', fieldName: 'date'},
+    {columnName: 'Time', fieldName: 'time'},
+    {columnName: "Temperature", fieldName: 'temperature'},
+]
+const tableForecast = new Table ("table-section", "Forecast", schema);
+const data = new DataForm("form-section", 16, cities);
+data.addFormHandler(displayTemperatures);
--- src/service/DataProcessor.js
+++ src/service/DataProcessor.js
@@ -15,31 +15,25 @@ export class DataProcessor {
         return getObjectFromResponse(obj, hourFrom, hourTo);
         }
 }
-function getObjectFromResponse(obj, hourFrom, hourTo){
-    const temperturePerHourPerDate = [];
-    let i = hourFrom;
-    let end = obj.hourly.time.length - (24 - hourTo);
-    while (i <= end){
-        const hourObject = {};
-        hourObject['date'] = obj.hourly.time[i].substring(0,10);
-        hourObject['time'] = obj.hourly.time[i].substring(11);
-        hourObject['temperature'] = obj.hourly.temperature_2m[i];
-        temperturePerHourPerDate.push(hourObject);
-        i++;
-    }
-    return temperturePerHourPerDate;
-    }
+
    
-    // function processRawData(rawData, hourFrom, hourTo) {
+    function getObjectFromResponse (rawData, hourFrom, hourTo) {
     
-    //     const timeArray = getHoursElements(rawData.hourly.time, hourFrom, hourTo);
-    //     const temperatureArray =getHoursElements(rawData.hourly.temperature_2m, hourFrom, hourTo); ;
-    //     return timeArray.map((t, index) => {
-    //         const res = {};
-    //         const dateTime = t.split("T");
-    //         res.date = dateTime[0];
-    //         res.hour = dateTime[1];
-    //         res.temperature = temperatureArray[index];
-    //         return res;
+        const timeArray = getHoursElements(rawData.hourly.time, hourFrom, hourTo);
+        const temperatureArray =getHoursElements(rawData.hourly.temperature_2m, hourFrom, hourTo); ;
+        return timeArray.map((t, index) => {
+            const res = {};
+            const dateTime = t.split("T");
+            res.date = dateTime[0];
+            res.time = dateTime[1];
+            res.temperature = temperatureArray[index];
+            return res;
     
-    //     })
+        })
+    }
+    function getHoursElements (array, hourFrom, hourTo) {
+        return array.filter((__, index) => {
+            const hour = index % 24;
+            return hour >= hourFrom && hour <= hourTo
+        } )
+    }
